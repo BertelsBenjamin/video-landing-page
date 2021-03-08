cf1a3caee3bce17459fcdc6d03660244151315a7
Benjamin Bertels <bertels123@gmail.com>
2021-03-08 09:04:47 +0100

    gh-pages setup

--- js/app.js
+++
@@ -1,8 +0,0 @@
-/* MENU TOGGLE */
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
-
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
-});
